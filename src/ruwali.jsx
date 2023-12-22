3a1f083af766acaf38af60c283b8ba6885237149
jayantiruwali <jayantiruwali999@gmail.com>
2023-12-22 09:38:15 +0545

    insertcoffeimage

--- src/ruwali.jsx
+++ src/ruwali.jsx
@@ -5,7 +5,7 @@ function Ruwali (){
     
     
     <p>Ruwali</p>
-    
+    <img width={200} src="https://assets.rumsan.net/clients/recordnepal/QmdumrSXGzbu3jd3foHUAmkPWWeiVTCk49Nj8FGQC7qNBU" alt="" />
     </>
 }
 export default Ruwali;
